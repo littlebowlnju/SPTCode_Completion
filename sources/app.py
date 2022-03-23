df4040968be9c2258ed1a8f1b3596f132c8da927
181840090 <181840090@smail.nju.edu.cn>
2022-03-23 16:13:32 +0800

    Update the app.py and completion.py .

--- sources/app.py
+++ sources/app.py
@@ -23,57 +23,7 @@ db_key_query = 'query'
 db_key_result = 'result'
 batch_size = 1
 MODEL_MODE_GEN = 'bart_gen'
-
-"""
-def load_model(
-        trained_model: Union[BartForClassificationAndGeneration, str],
-        trained_vocab: Union[Tuple[Vocab, Vocab, Vocab], str]
-):
-    # -------------------------------
-    # vocabs
-    # -------------------------------
-    global code_vocab
-    global model
-    if trained_vocab:
-        if isinstance(trained_vocab, tuple):
-            # app.info('Vocabularies are passed through parameter')
-            assert len(trained_vocab) == 3
-            code_vocab, ast_vocab, nl_vocab = trained_vocab
-        else:
-            # app.info('Loading vocabularies from files')
-            code_vocab = load_vocab(vocab_root=trained_vocab, name='code')
-            ast_vocab = load_vocab(vocab_root=trained_vocab, name='ast')
-            nl_vocab = load_vocab(vocab_root=trained_vocab, name='nl')
-
-    # app.info(f'The size of code vocabulary: {len(code_vocab)}')
-    # app.info(f'The size of nl vocabulary: {len(nl_vocab)}')
-    # app.info(f'The size of ast vocabulary: {len(ast_vocab)}')
-    # app.info('Vocabularies built successfully')
-
-    # ------------------------------
-    # model
-    # ------------------------------
-    # app.info('-' * 100)
-    if trained_model:
-        if isinstance(trained_model, BartForClassificationAndGeneration):
-            # app.info('Model is passed through parameter')
-            model = trained_model
-        else:
-            # app.info('Loading the model from file')
-            config = BartConfig.from_json_file(os.path.join(trained_model, 'config.json'))
-            model = BartForClassificationAndGeneration.from_pretrained(os.path.join(trained_model, 'pytorch_model.bin'),
-                                                                       config=config)
-    model.set_model_mode(MODEL_MODE_GEN)
-    # log model statistic
-    # app.info('Trainable parameters: {}'.format(human_format(count_params(model))))
-    table = layer_wise_parameters(model)
-    # app.debug('Layer-wised trainable parameters:\n{}'.format(table))
-    # app.info('Model built successfully')
-    return model, code_vocab
-
-
-model, code_vocab = load_model(trained_model='../pre_trained/models/all/', trained_vocab='../pre_trained/vocabs/')
-"""
+args = None
 
 # ------ original source ------------
 # SOURCE = "public int[] twoSum(int[] nums, int target) {" \
@@ -120,29 +70,14 @@ def generate_ast_nl_test():
 
 @app.route("/", methods=['POST', 'GET'])
 def generate_result():
-    from completion import run_completion
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.register('type', 'bool', lambda v: v.lower() in ['yes', 'true', 't', '1', 'y'])
-
-    add_args(parser)
-    # args = parser.parse_args()
-    args, unknown = parser.parse_known_args()
-
-    args.output_root = os.path.join(
-        '..',
-        'outputs',
-        '{}_{}'.format(args.model_name, time.strftime('%Y%m%d_%H%M%S', time.localtime())))
-    args.checkpoint_root = os.path.join(args.output_root, 'checkpoints')
-    args.tensor_board_root = os.path.join(args.output_root, 'runs')
-
+    from completion import complete
     if request.method == 'POST':
         data = request.get_data().decode("utf-8")
-        print(args)
-        predictions, prediction_scores = run_completion(
+        print(data)
+        #print(args)
+        predictions, prediction_scores = complete(
             args=args,
             source=data,
-            trained_model=args.trained_model,
-            trained_vocab=args.trained_vocab
         )
         pre_scores = []
         for score in prediction_scores:
@@ -155,41 +90,6 @@ def generate_result():
     else:
         return jsonify({"about": "Hello World"})
 
-
-'''
-def handle_query():
-    text = request.get_json()  # 获取用户query中的文本 例如"I love you"
-    id_ = str(uuid.uuid1())  # 为query生成唯一标识
-    d = {'id': id_, 'text': text}  # 绑定文本和query id
-    redis_.rpush(db_key_query, json.dumps(d))  # 加入redis
-    while True:
-        result = redis_.get(id_)  # 获取该query的模型结果
-        # print(result)
-        if result is not None:
-            redis_.delete(id_)
-            result_text = {'code': "200", 'data': result}
-            break
-    return jsonify(result_text)  # 返回结果
-
-
-def print_test(batch_size):
-    while True:
-        texts = []
-        query_ids = []
-        if redis_.llen(db_key_query) == 0:  # 若队列中没有元素就继续获取
-            continue
-        print(redis_.llen(db_key_query))
-        for i in range(min(redis_.llen(db_key_query), batch_size)):
-            query = redis_.lpop(db_key_query).decode('UTF-8')  # 获取query的text
-            print(query)
-            query_ids.append(json.loads(query)['id'])
-            texts.append(json.loads(query)['text'])  # 拼接若干text 为batch
-        result = {'label': 'POSITIVE', 'score': 0.99893874}  # 调用模型
-        res = {'label': '', 'score': 0}
-        for (id_, res) in zip(query_ids, result):
-            res['score'] = str(res['score'])
-            redis_.set(id_, json.dumps(res))  # 将模型结果送回队列
-'''
 
 # to see what dataset item like
 # def dataset_item_test():
@@ -207,6 +107,20 @@ def print_test(batch_size):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.register('type', 'bool', lambda v: v.lower() in ['yes', 'true', 't', '1', 'y'])
+
+    add_args(parser)
+    # args = parser.parse_args()
+    args, unknown = parser.parse_known_args()
+
+    args.output_root = os.path.join(
+        '..',
+        'outputs',
+        '{}_{}'.format(args.model_name, time.strftime('%Y%m%d_%H%M%S', time.localtime())))
+    args.checkpoint_root = os.path.join(args.output_root, 'checkpoints')
+    args.tensor_board_root = os.path.join(args.output_root, 'runs')
+
     # tokenize_source_test()
 
     # generate_ast_nl_test()
--- sources/completion.py
+++ sources/completion.py
@@ -1,178 +1,16 @@
-from typing import Union, Tuple
-from transformers import BartConfig, Seq2SeqTrainingArguments, EarlyStoppingCallback, \
-    IntervalStrategy, SchedulerType
-
 import logging
-import os
 import torch
-
-from data.vocab import Vocab, load_vocab, init_vocab
-from model.bart import BartForClassificationAndGeneration
-from utils.general import count_params, human_format, layer_wise_parameters
-from eval.metrics import bleu, meteor, rouge_l, avg_ir_metrics, accuracy_for_sequence, accuracy_top_k_for_sequence
-from utils.trainer import CodeTrainer
-from utils.callbacks import LogStateCallBack
 from data.data_utils import generate_input, my_parse_for_completion
 
 MODEL_MODE_GEN = 'bart_gen'
 
 logger = logging.getLogger(__name__)
 
-
-def run_completion(
-        args,
-        source,
-        trained_model: Union[BartForClassificationAndGeneration, str],
-        trained_vocab: Union[Tuple[Vocab, Vocab, Vocab], str]
-):
-    """
-    Run code completion, given source code, should give predictions list
-    :param args: arguments
-    :param source: str, source code string
-    :param trained_model: trained model,
-    :param trained_vocab: trained vocab
-    :return: list[str], list of predictions with highest probability, len == 5
-    """
-    # -------------------------------
-    # vocabs
-    # -------------------------------
-    if trained_vocab:
-        if isinstance(trained_vocab, tuple):
-            logger.info('Vocabularies are passed through parameter')
-            assert len(trained_vocab) == 3
-            code_vocab, ast_vocab, nl_vocab = trained_vocab
-        else:
-            logger.info('Loading vocabularies from files')
-            code_vocab = load_vocab(vocab_root=trained_vocab, name='code')
-            ast_vocab = load_vocab(vocab_root=trained_vocab, name='ast')
-            nl_vocab = load_vocab(vocab_root=trained_vocab, name='nl')
-
-    logger.info(f'The size of code vocabulary: {len(code_vocab)}')
-    logger.info(f'The size of nl vocabulary: {len(nl_vocab)}')
-    logger.info(f'The size of ast vocabulary: {len(ast_vocab)}')
-    logger.info('Vocabularies built successfully')
-
-    # ------------------------------
-    # model
-    # ------------------------------
-    logger.info('-' * 100)
-    if trained_model:
-        if isinstance(trained_model, BartForClassificationAndGeneration):
-            logger.info('Model is passed through parameter')
-            model = trained_model
-        else:
-            logger.info('Loading the model from file')
-            config = BartConfig.from_json_file(os.path.join(trained_model, 'config.json'))
-            model = BartForClassificationAndGeneration.from_pretrained(os.path.join(trained_model, 'pytorch_model.bin'),
-                                                                       config=config)
-    model.set_model_mode(MODEL_MODE_GEN)
-    # log model statistic
-    logger.info('Trainable parameters: {}'.format(human_format(count_params(model))))
-    table = layer_wise_parameters(model)
-    logger.debug('Layer-wised trainable parameters:\n{}'.format(table))
-    logger.info('Model built successfully')
-
-    # --------------------------------------------------
-    # trainer
-    # --------------------------------------------------
-    logger.info('-' * 100)
-    logger.info('Initializing the running configurations')
-
-    def decode_preds(preds):
-        preds, labels = preds
-        decoded_preds = code_vocab.decode_batch(preds)
-        decoded_labels = code_vocab.decode_batch(labels)
-        return decoded_labels, decoded_preds
-
-    # compute metrics
-    def compute_valid_metrics(eval_preds):
-        decoded_labels, decoded_preds = decode_preds(eval_preds)
-        refs = [ref.strip().split() for ref in decoded_labels]
-        cans = [can.strip().split() for can in decoded_preds]
-        result = {}
-        result.update(bleu(references=refs, candidates=cans))
-        result.update(accuracy_for_sequence(references=refs, candidates=cans))
-        return result
-
-    def compute_test_metrics(eval_preds):
-        decoded_labels, decoded_preds = decode_preds(eval_preds)
-        result = {'references': decoded_labels, 'candidates': decoded_preds}
-        refs = [ref.strip().split() for ref in decoded_labels]
-        cans = [can.strip().split() for can in decoded_preds]
-        result.update(bleu(references=refs, candidates=cans))
-        try:
-            result.update(meteor(references=refs, candidates=cans))
-        except Exception:
-            pass
-        result.update(rouge_l(references=refs, candidates=cans))
-        result.update(avg_ir_metrics(references=refs, candidates=cans))
-        result.update(accuracy_for_sequence(references=refs, candidates=cans))
-        return result
-
-    training_args = Seq2SeqTrainingArguments(output_dir=os.path.join(args.checkpoint_root, 'completion'),
-                                             overwrite_output_dir=False,
-                                             do_train=True,
-                                             do_eval=True,
-                                             do_predict=True,
-                                             evaluation_strategy=IntervalStrategy.EPOCH,
-                                             prediction_loss_only=False,
-                                             per_device_train_batch_size=args.batch_size,
-                                             per_device_eval_batch_size=args.eval_batch_size,
-                                             gradient_accumulation_steps=args.gradient_accumulation_steps,
-                                             learning_rate=args.learning_rate,
-                                             weight_decay=args.lr_decay_rate,
-                                             max_grad_norm=args.grad_clipping_norm,
-                                             num_train_epochs=args.n_epoch,
-                                             lr_scheduler_type=SchedulerType.LINEAR,
-                                             warmup_steps=args.warmup_steps,
-                                             logging_dir=os.path.join(args.tensor_board_root, 'completion'),
-                                             logging_strategy=IntervalStrategy.STEPS,
-                                             logging_steps=args.logging_steps,
-                                             save_strategy=IntervalStrategy.EPOCH,
-                                             save_total_limit=2,
-                                             seed=args.random_seed,
-                                             fp16=args.fp16,
-                                             dataloader_drop_last=False,
-                                             run_name=args.model_name,
-                                             load_best_model_at_end=True,
-                                             metric_for_best_model='accuracy',
-                                             greater_is_better=True,
-                                             ignore_data_skip=False,
-                                             label_smoothing_factor=args.label_smoothing,
-                                             report_to=['tensorboard'],
-                                             dataloader_pin_memory=True,
-                                             predict_with_generate=True)
-    trainer = CodeTrainer(main_args=args,
-                          code_vocab=code_vocab,
-                          ast_vocab=ast_vocab,
-                          nl_vocab=nl_vocab,
-                          task='completion',
-                          model=model,
-                          args=training_args,
-                          data_collator=None,
-                          train_dataset=None,
-                          eval_dataset=None,
-                          tokenizer=nl_vocab,
-                          model_init=None,
-                          compute_metrics=compute_valid_metrics,
-                          callbacks=[
-                              EarlyStoppingCallback(early_stopping_patience=args.early_stop_patience),
-                              LogStateCallBack()])
-    logger.info('Running configurations initialized successfully')
-
-    # -----------------------------
-    # generate model input from source code
-    # -----------------------------
+def complete(args, source):
     inputs = generate_input(args, source)
-    # inputs = my_parse_for_completion(args, './dataset/source.txt', './dataset/target.txt')
 
-    # -----------------------------
-    # predict
-    # -----------------------------
-    predictions = []
-    prediction_scores = []
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    outputs = model.generate(
+    outputs = args.model.generate(
         input_ids=inputs['input_ids'].to(device),
         attention_mask=inputs['attention_mask'].to(device),
         max_length=args.completion_max_len,
@@ -183,11 +21,12 @@ def run_completion(
         output_scores=True,
         return_dict_in_generate=True,
     )
+
     output_seqs = outputs.sequences
     output_seq_scores = outputs.sequences_scores
     output_seqs = output_seqs.view(1, -1, output_seqs.size(-1))
     for output in output_seqs:
-        predictions = code_vocab.decode_batch(output.cpu().numpy())  # decode to strings
+        predictions = args.trained_vocab[0].decode_batch(output.cpu().numpy())  # decode to strings
     print('--------------- predictions -----------------')
     print(predictions)
     print('--------------- predictions‘ scores ----------------')
