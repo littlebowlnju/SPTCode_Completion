c604e8a6638a2ff245e3612e17dc3f46bcd86fbb
littlebowl <littlebowlnju@163.com>
2022-03-29 23:15:44 +0800

    request can send and get using queue

---
+++ sources/app_mp.py
@@ -0,0 +1,102 @@
+# from multiprocessing import Queue
+import logging
+import redis
+from flask import Flask, request
+import json
+from concurrency.task import CodeCompletionTask
+import time
+import queue
+import argparse
+from args import add_args
+from transformers import BartConfig
+from data.vocab import load_vocab
+from model.bart import BartForClassificationAndGeneration
+import os
+import enums
+from completion import complete
+import threading
+
+
+logger = logging.getLogger(__name__)
+app = Flask(__name__)
+pool = redis.ConnectionPool(host='localhost', port=6379, max_connections=50)
+redis_ = redis.Redis(connection_pool=pool, decode_responses=True)
+
+db_key_query = 'query'
+db_key_result = 'result'
+batch_size = 1
+MODEL_MODE_GEN = 'bart_gen'
+args = None
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.register('type', 'bool', lambda v: v.lower() in ['yes', 'true', 't', '1', 'y'])
+
+add_args(parser)
+# args = parser.parse_args()
+args, unknown = parser.parse_known_args()
+
+args.output_root = os.path.join(
+    '..',
+    'outputs',
+    '{}_{}'.format(args.model_name, time.strftime('%Y%m%d_%H%M%S', time.localtime())))
+args.checkpoint_root = os.path.join(args.output_root, 'checkpoints')
+args.tensor_board_root = os.path.join(args.output_root, 'runs')
+
+code_vocab = load_vocab(vocab_root=args.trained_vocab, name=args.code_vocab_name)
+
+config = BartConfig.from_json_file(os.path.join(args.trained_model, 'config.json'))
+model = BartForClassificationAndGeneration.from_pretrained(os.path.join(args.trained_model, 'pytorch_model.bin'),
+                                                           config=config)
+model.set_model_mode(enums.MODEL_MODE_GEN)
+req_queue = queue.Queue()
+
+
+@app.before_first_request
+def poll():
+    def check_queue():
+        while True:
+            if not req_queue.empty():
+                print("req_queue size: ", req_queue.qsize())
+                size = min(req_queue.qsize(), 100)
+                task_list = []
+                for _ in range(size):
+                    task_list.append(req_queue.get())
+
+                for i in range(size):
+                    predictions, prediction_scores = complete(
+                        args=args,
+                        source=task_list[i].code,
+                        model=model,
+                        code_vocab=code_vocab
+                    )
+                    pre_scores = []
+                    for score in prediction_scores:
+                        pre_scores.append("%.2f%%" % (score * 100))
+                    result = {
+                        "predictions": predictions,
+                        "prediction_scores": pre_scores
+                    }
+                    task_list[i].set_result(result)
+            time.sleep(5)  # polling every 5 seconds
+
+    thread = threading.Thread(target=check_queue)
+    thread.start()
+
+
+@app.route("/", methods=["POST"])
+def generate_result_mp():
+    # print("------------------request received-------------------")
+    data = request.get_data().decode("utf-8")
+    task = CodeCompletionTask(data)
+    req_queue.put(task)
+    result = task.get_result()
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    # app.run(host="172.29.7.224", port=1818, debug=True)
+    from werkzeug.middleware.proxy_fix import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.run()
+
+
---
+++ sources/concurrency/task.py
@@ -0,0 +1,21 @@
+import threading
+
+
+class CodeCompletionTask:
+    def __init__(self, code):
+        self.code = code
+        self.result = {}
+        self.lock = threading.Condition()
+
+    def get_result(self):
+        print("get result")
+        with self.lock:
+            self.lock.wait()
+            print("getting result waiting!!!")
+        return self.result
+
+    def set_result(self, result_dic):
+        print("set result")
+        with self.lock:
+            self.result = result_dic
+            self.lock.notify()
